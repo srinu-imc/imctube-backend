a8adb25042cb55deaa0b334b00a9d624dc30e149
Srinivasa Reddy Vundela <vundela.srinu@gmail.com>
2016-03-12 15:19:36 -0800

    Tools for getting movies/artists with are duplicates/nothumbnails

--- src/main/java/com/imctube/cinema/resource/ToolResource.java
+++ src/main/java/com/imctube/cinema/resource/ToolResource.java
@@ -1,9 +1,11 @@
 package com.imctube.cinema.resource;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,13 +14,16 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
-import com.imctube.cinema.db.utils.Authorize;
+import com.imctube.cinema.model.Artist;
 import com.imctube.cinema.model.Movie;
 import com.imctube.cinema.service.ArtistService;
 import com.imctube.cinema.service.MovieService;
 
+import jersey.repackaged.com.google.common.collect.Lists;
+import jersey.repackaged.com.google.common.collect.Sets;
+
 @Path("tools")
-//@Authorize
+// @Authorize
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ToolResource {
@@ -36,14 +41,80 @@ public class ToolResource {
         }
     }
 
+    @GET
+    @Path("/movies/dups/videoids")
+    public List<Movie> getDuplicateVideoIds() {
+        Set<String> videoIds = Sets.newHashSet();
+        List<Movie> dups = Lists.newArrayList();
+        for (Movie movie : movieService.getAllMovies()) {
+            if (videoIds.contains(movie.getVideoId())) {
+                Movie dupMovie = new Movie();
+                dupMovie.setVideoId(movie.getVideoId());
+                dups.add(dupMovie);
+            } else {
+                videoIds.add(movie.getVideoId());
+            }
+        }
+        return dups;
+    }
+
+    @GET
+    @Path("/artists/dups")
+    public List<Artist> getDuplicateArtists() {
+        Set<String> artistNames = Sets.newHashSet();
+        List<Artist> dups = Lists.newArrayList();
+        for (Artist artist : artistService.getArtists(false)) {
+            if (artistNames.contains(artist.getIndustryName().toLowerCase())) {
+                Artist dupArtist = new Artist();
+                dupArtist.setIndustryName(artist.getIndustryName());
+                dups.add(dupArtist);
+            } else {
+                artistNames.add(artist.getIndustryName().toLowerCase());
+            }
+        }
+        return dups;
+    }
+
+    @GET
+    @Path("/movies/dups/movies")
+    public List<Movie> getDuplicateMovieNames() {
+        Set<String> movieNames = Sets.newHashSet();
+        List<Movie> dups = Lists.newArrayList();
+        for (Movie movie : movieService.getAllMovies()) {
+            if (movieNames.contains(movie.getName().toLowerCase())) {
+                Movie dupMovie = new Movie();
+                dupMovie.setName(movie.getName());
+                dups.add(dupMovie);
+            } else {
+                movieNames.add(movie.getName().toLowerCase());
+            }
+        }
+        return dups;
+    }
+
+    @GET
+    @Path("/movies/nothumbnails")
+    public List<Movie> getNoThumbnails() {
+        List<Movie> movieIds = Lists.newArrayList();
+        for (Movie movie : movieService.getAllMovies()) {
+            if (movie.getThumbnailCount() == 0) {
+                Movie dupMovie = new Movie();
+                dupMovie.setVideoId(movie.getVideoId());
+                movieIds.add(dupMovie);
+            }
+        }
+        return movieIds;
+    }
+
     @PUT
     @Path("/movies/{videoId}")
     public void updateThumbnailCount(@Context final HttpServletRequest request, @PathParam("videoId") String videoId,
             @QueryParam("count") int count) {
 
         Movie movie = movieService.getMovieByVideoId(videoId);
-        movie.setThumbnailCount(count);
-        movieService.updateMovie(movie.getId(), movie);
+        if (movie.getThumbnailCount() == 0) {
+            movie.setThumbnailCount(count);
+            movieService.updateMovie(movie.getId(), movie);
+        }
     }
-
 }
